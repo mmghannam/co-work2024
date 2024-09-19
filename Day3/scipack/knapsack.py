a5d0aaba59317e3c03d314c4605223a87656b568
FabianBal <165779234+FabianBal@users.noreply.github.com>
2024-09-19 15:44:39 +0000

    restore initial file

--- Day3/scipack/knapsack.py
+++ Day3/scipack/knapsack.py
@@ -1,5 +1,4 @@
 from typing import List
-import pyscipopt as scip
 
 
 def pricing_solver(sizes: List[int], capacity: int, dual_solution: dict[float], together: set[tuple[int, int]],
@@ -41,26 +40,9 @@ def solve_knapsack(sizes: List[int], values: List[float], capacity: int) -> tupl
     Returns:
     tuple[float, List[int]] - the optimal value and the pattern of the items
     """
-    n = len(sizes)
-    model = scip.Model("knapsack_mip")
-    # Create binary variables for each item
-    x = [model.addVar(vtype='B', name=f'x{i}') for i in range(n)]
 
-    # Set the objective function (maximize total value)
-    model.setObjective(scip.quicksum(values[i] * x[i] for i in range(n)), sense="maximize")
+    raise NotImplementedError("The knapsack solver is not implemented yet")
 
-    # Add the knapsack constraint (total weight <= capacity)
-    model.addCons(scip.quicksum(sizes[i] * x[i] for i in range(n)) <= capacity, name='knapsack_constraint')
-
-    # Optimize the model
-    model.optimize()
-
-     # Get the optimal value and selected items
-    opt_value = model.getObjVal()
-
-    selected_items = [i for i in range(n) if model.getVal(x[i]) == 1]
-
-    return tuple([opt_value, selected_items])
 
 def solve_knapsack_with_constraints(
         sizes: List[int], values: List[float], capacity: int, together: set[tuple[int, int]],

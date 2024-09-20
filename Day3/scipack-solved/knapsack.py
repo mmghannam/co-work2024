9107d59833f0c923bb016e6a850ed976709207e7
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2024-09-20 10:07:01 +0200

    Add solution to scipack-solved

--- Day3/scipack-solved/knapsack.py
+++ Day3/scipack-solved/knapsack.py
@@ -1,20 +1,22 @@
 from typing import List
 
+from pyscipopt import Model
+
 
 def pricing_solver(sizes: List[int], capacity: int, dual_solution: dict[float], together: set[tuple[int, int]],
                    apart: set[tuple[int, int]]) -> tuple[float, List[int]]:
     """
     Solve the pricing problem for the knapsack problem (with branching constraints)
-    
+
     Parameters:
     sizes: List[int] - the sizes of the items
     capacity: int - the capacity of the knapsack
     dual_solution: dict[float] - the dual solution of the linear relaxation
     together: set[tuple[int]] - the pairs of items that must be together
     apart: set[tuple[int]] - the pairs of items that must be apart
-    
+
     Returns:
-    tuple[float, List[int]] - the minimum reduced cost and the pattern of the items    
+    tuple[float, List[int]] - the minimum reduced cost and the packing of the items
     """
 
     profits = [dual_solution[i] for i in range(len(sizes))]
@@ -30,7 +32,7 @@ def pricing_solver(sizes: List[int], capacity: int, dual_solution: dict[float], 
 
 def solve_knapsack(sizes: List[int], values: List[float], capacity: int) -> tuple[float, List[int]]:
     """
-    Solve the knapsack problem with branching constraints
+    Solve the knapsack problem
 
     Parameters:
     sizes: List[int] - the sizes of the items
@@ -38,10 +40,22 @@ def solve_knapsack(sizes: List[int], values: List[float], capacity: int) -> tupl
     capacity: int - the capacity of the knapsack
 
     Returns:
-    tuple[float, List[int]] - the optimal value and the pattern of the items
+    tuple[float, List[int]] - the optimal value and the packing of the items
     """
 
-    raise NotImplementedError("The knapsack solver is not implemented yet")
+    m = Model("knapsack")
+    m.hideOutput()
+    x = {}
+    for i in range(len(sizes)):
+        x[i] = m.addVar(vtype="B", name=f"x{i}", obj=values[i])
+
+    m.addCons(sum(sizes[i] * x[i] for i in range(len(sizes))) <= capacity)
+
+    m.setMaximize()
+    m.optimize()
+
+    packing = [i for i in range(len(sizes)) if m.getVal(x[i]) > 0.5]
+    return m.getObjVal(), packing
 
 
 def solve_knapsack_with_constraints(
@@ -59,7 +73,27 @@ def solve_knapsack_with_constraints(
     apart: set[tuple[int]] - the pairs of items that must be apart
 
     Returns:
-    tuple[float, List[int]] - the optimal value and the pattern of the items
+    tuple[float, List[int]] - the optimal value and the packing of the items
     """
 
-    raise NotImplementedError("The knapsack solver with constraints is not implemented yet")
+    m = Model("knapsack_with_constraints")
+    m.hideOutput()
+
+    x = {}
+    for i in range(len(sizes)):
+        x[i] = m.addVar(vtype="B", name=f"x{i}", obj=values[i])
+
+    m.addCons(sum(sizes[i] * x[i] for i in range(len(sizes))) <= capacity)
+
+    for i, j in together:
+        m.addCons(x[i] == x[j])
+
+    for i, j in apart:
+        m.addCons(x[i] + x[j] <= 1)
+
+    m.setMaximize()
+
+    m.optimize()
+
+    packing = [i for i in range(len(sizes)) if m.getVal(x[i]) > 0.5]
+    return m.getObjVal(), packing
--- Day3/scipack-solved/ryan_foster.py
+++ Day3/scipack-solved/ryan_foster.py
@@ -1,60 +1,59 @@
 from typing import List
 from pyscipopt import Branchrule, SCIP_RESULT
 
+
 class RyanFoster(Branchrule):
     def __init__(self, *args, **kwargs):
-        """ 
+        """
         Branching decisions are stored in a dictionary, where the key is the node number
         and the value is a dictionary with the keys "together" and "apart"
         the value of "together" is a set of pairs of items that must be in the same bin
-        the value of "apart" is a set of pairs of items that must be in different bins. 
+        the value of "apart" is a set of pairs of items that must be in different bins.
         """
         super().__init__(*args, **kwargs)
         self.branching_decisions = {
-            1: { # root node
+            1: {  # root node
                 "together": set(),
                 "apart": set(),
             }
         }
-    
+
     def branchexeclp(self, allowaddcons):
         # get the fractional variables from the LP solution
         lpcands, lpcandssol, *_ = self.model.getLPBranchCands()
-        
+
         patterns_with_vals = [
             (eval(var.name.replace("t_", "")), val) for var, val in zip(lpcands, lpcandssol)
         ]
-        
+
+        # TODO (Exercise 2: choose a fractional pair to branch on)
         chosen_pair = choose_fractional_pair(patterns_with_vals)
-        
+
         parent_together = set()
         parent_apart = set()
-        
+
         # get the branching decisions of the parent node
         parent = self.model.getCurrentNode()
         if parent:
             parent_together = set(self.branching_decisions[parent.getNumber()]["together"])
             parent_apart = set(self.branching_decisions[parent.getNumber()]["apart"])
-        
-                                
+
         # Left subproblem: enforce that pair is in the same bin
         left_node = self.model.createChild(0, 0)
-        
-        raise NotImplementedError("Complete the following: what should be the value of together and apart?")
-        # self.branching_decisions[left_node.getNumber()] = {
-        #     "together": ?,
-        #     "apart": ?
-        # }
+
+        self.branching_decisions[left_node.getNumber()] = {
+            "together": parent_together.union({chosen_pair}),
+            "apart": parent_apart
+        }
 
         # Right subproblem: enforce that pair is in different bins
         right_node = self.model.createChild(0, 0)
 
-        raise NotImplementedError("Complete the following: what should be the value of together and apart?")
-        # self.branching_decisions[right_node.getNumber()] = {
-            # "together": ?,
-            # "apart": ?
-        # }
-        
+        self.branching_decisions[right_node.getNumber()] = {
+            "together": parent_together,
+            "apart": parent_apart.union({chosen_pair})
+        }
+
         return {"result": SCIP_RESULT.BRANCHED}
 
 
@@ -69,19 +68,30 @@ def all_fractional_pairs(patterns_with_vals: List[tuple[List[int], float]]) -> L
     List[tuple[int, int]] - a list of pairs of items that are fractional in the LP solution
     """
 
-    raise NotImplementedError("Implement this function")
+    pairs = {}
+    for pattern, val in patterns_with_vals:
+        for i in range(len(pattern)):
+            for j in range(i + 1, len(pattern)):
+                if pattern[i] < pattern[j]:
+                    tuple = (pattern[i], pattern[j])
+                    if tuple not in pairs:
+                        pairs[tuple] = val
+                    else:
+                        pairs[tuple] += val
+
+    return [pair for pair, val in pairs.items() if 1e-6 < val < 1 - 1e-6]
 
 
 def choose_fractional_pair(patterns_with_vals: List[tuple[List[int], float]]) -> tuple[int, int]:
     """
     Choose a fractional pair to branch on
-    
+
     Parameters:
     fractional_vars: List[tuple[List[int], float]] - a list of packings and the value of the variable in the LP solution
-    
+
     Returns:
     tuple[int, int] - the pair of items to branch on
     """
-    
+
     first_pair = all_fractional_pairs(patterns_with_vals)[0]
     return first_pair
